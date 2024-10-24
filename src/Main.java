8c469293703c426c61afd6eaf88a5d12a3908783
Ivan-Castill <ivan2003castillo12345@gmail.comzx>
2024-10-24 09:57:58 -0500

    clase 24/10/2024

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Operaciones suma1=new Operaciones(3.7,7.8);
+        suma1.imprimir();
+        //suma1.imprimirsuma();//tercera forma
+       //suma1.imprimirresta();
+        //suma1.Sumar();//primera forma
+        //double result=suma1.Sumar();//segunda forma de imprimir
+        //System.out.print("La suma es "+result);
+        //System.out.print("La suma es "+suma1.Sumar());//primera forma
     }
 }
--- src/Operaciones.java
+++ src/Operaciones.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+public class Operaciones {
 
-public class operaciones {
+    double num1,num2;
+    double suma;
+
+    //metodo consturctor
+    public Operaciones (double num1,double num2){
+        this.num1=num1;
+        this.num2=num2;
+    }
+    //metodo personalizado
+    public double Sumar(){
+        return num1+num2;
+    }
+    public double resta(){
+        return num1-num2;
+    }
+    public void imprimir(){
+        System.out.print("la suma desde el metodo personalizado es: "+Sumar());
+        System.out.print("\nla resta desde el metodo personalizado es: "+resta());
+    }
+    //public void imprimirsuma(){
+     //   System.out.print("la suma desde el metodo personalizado es: "+Sumar());
+   // }
+    //public void imprimirresta(){
+      //  System.out.print("\nla resta desde el metodo personalizado es: "+resta());
+    //}
+    //un objeto
+    //        ATRIBUTOS NUM1,NUM2
+      //      METODOS CONTRUCTOR
+        //    METODO PERSONALIZADO
 }
